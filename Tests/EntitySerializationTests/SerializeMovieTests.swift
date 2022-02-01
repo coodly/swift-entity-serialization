1097f815e328dd226b39f2cde1c0a75a6fed9164
Jaanus Siim <jaanus@jaanussiim.com>
2022-02-01 10:07:06 +0200

    Check the serialised record

--- Tests/EntitySerializationTests/SerializeMovieTests.swift
+++ Tests/EntitySerializationTests/SerializeMovieTests.swift
@@ -15,7 +15,18 @@ final class SerializeMovieTests: XCTestCase {
         let serialize = CloudKitSerialize(serlialize: [.movie])
         let records = serialize.serialize(entities: [movie], in: CKRecordZone.default())
         XCTAssertEqual(1, records.count)
-        dump(records)
+        guard let record = records.first else {
+            return
+        }
+        
+        XCTAssertEqual("Movie", record.recordType)
+        XCTAssertEqual("move-1234", record.recordID.recordName)
+        XCTAssertEqual([1, 2, 3, 4], record["genres"] as? [Int])
+        XCTAssertEqual("Test movie one", record["name"] as? String)
+        XCTAssertEqual(NSNumber(value: 7), record["rating"] as? NSNumber)
+        XCTAssertEqual(NSNumber(value: 8), record["runtime"] as? NSNumber)
+        XCTAssertEqual(NSNumber(value: 1234), record["tmdbID"] as? NSNumber)
+        XCTAssertEqual(NSNumber(value: 2022), record["year"] as? NSNumber)
     }
 }
 
